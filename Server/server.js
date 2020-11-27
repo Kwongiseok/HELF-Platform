9b8d639d60b5b7f1cd129895448df7036c632448
Kwongiseok <ri2377@naver.com>
2020-11-27 14:39:05 +0900

    server room List

--- Server/server.js
+++ Server/server.js
@@ -19,7 +19,7 @@ const users = {};
 const socketToRoom = {};
 let rooms = [];
 let roomIDs = new Map();
-let roomGets = [];
+let roomGets = []; // map 형태로 데이터를 쏠 때 안돼서 일단 배열형태로 진행
 
 app.get('/api/roomList' , async(req,res) => {
     res.send({roomGets,rooms});
@@ -47,6 +47,7 @@ io.on('connection', socket => {
         socketToRoom[socket.id] = roomID;
         const usersInThisRoom = users[roomID].filter(id => id !== socket.id);
         socket.emit("all users", usersInThisRoom);
+        console.log(users[roomID]);
     });
 
     socket.on("sending signal", payload => {
@@ -65,12 +66,17 @@ io.on('connection', socket => {
             users[roomID] = room;
 
         }
+        socket.broadcast.emit('user left',socket.id);
         // 사용자가 1명도 없는 방은 없애버리는 기능 구현해야함
-        // if (room.length < 1) { // 캠 켜지기전에 닫으면 서버가 죽는다 => 에러 고치기전에 사용 X
-        //     const roomname_tmp = roomIDs.get(roomID); // roomName 검색
-        //     const rooms_tmp = rooms.filter(roomName => roomName !== roomname_tmp); //방이름 같은 거 예외처리 해야함
-        //     rooms = rooms_tmp;
-        // }
+        if (room && room.length < 1) { // 캠 켜지기전에 닫으면 서버가 죽는다 => 에러 고치기전에 사용 X
+            const roomname_tmp = roomIDs.get(roomID); // roomName 검색
+            const roomGetsDelAddr = roomGets[rooms.indexOf(roomname_tmp)]; // roomGets 주소를 찾아서 삭제하기 위함
+            const rooms_tmp = rooms.filter(roomName => roomName !== roomname_tmp); //방이름 같은 거 예외처리 해야함
+            rooms = rooms_tmp;
+
+            const roomGets_tmp = roomGets.filter(Addr => Addr !== roomGetsDelAddr);
+            roomGets = roomGets_tmp;
+        }
     });
 
 });
